a5932429d8da7e09c11f86e438de41389344cf97
bcreedon <52137962+bcreedon@users.noreply.github.com>
2019-07-30 02:49:05 -0700

    Add files via upload

---
+++ PeopleSearchAPI/PeopleSearchAPI/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleSearchAPI.Models
+{
+    public class Person
+    {
+        [Key]
+        public int RecID { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string Address { get; set; }
+        [Required]
+        [Column(TypeName = "int")]
+        public int Age{ get; set; }
+        [Required]
+        [Column(TypeName = "varchar(500)")]
+        public string Interests { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string ImageUrl { get; set; }
+    }
+}
